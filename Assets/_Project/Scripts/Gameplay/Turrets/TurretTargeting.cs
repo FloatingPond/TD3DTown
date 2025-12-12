036ba441bd9e1c27306827bf5538c1f84e98b97a
Paul Gush <Paul1208Gush@Gmail.com>
2025-12-12 16:24:00 +0000

    Targeting progress

--- Assets/_Project/Scripts/Gameplay/Turrets/TurretTargeting.cs
+++ Assets/_Project/Scripts/Gameplay/Turrets/TurretTargeting.cs
@@ -10,18 +10,51 @@ namespace _Project.Scripts.Gameplay.Turrets
         
         private Transform _currentTarget;
 
+        private void Start()
+        {
+            _targetingStrategy.Initialize(this);
+        }
+
         private void FixedUpdate()
         {
             if (_currentTarget == null)
             {
-                _targetingStrategy.GetTarget();
+                _currentTarget = _targetingStrategy.GetSingleTarget();
+                return;
             }
-        
+
+            if (IsCurrentTargetInRange())
+            {
+                RotateObjectTowardTarget();
+            }
+        }
+
+        private bool IsCurrentTargetInRange()
+        {
+            Vector3 dir = transform.position - _currentTarget.position;
+            if (!(dir.magnitude > _targetingStrategy.Range))
+                return true;
+
+            _currentTarget = null;
+            return false;
+
+        }
+
+        private void RotateObjectTowardTarget()
+        {
+            if (_objectToRotateTowardsTarget == null)
+                return;
+
             Vector3 dir = transform.position - _currentTarget.position; // inverted (away from target)
             dir.y = 0f; // only yaw (Y rotation)
 
             if (dir.sqrMagnitude > 0.0001f)
                 _objectToRotateTowardsTarget.rotation = Quaternion.LookRotation(dir, Vector3.up);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, _targetingStrategy.Range);
         }
     }
 }
--- Assets/_Project/Scripts/Gameplay/Turrets/TurretTargetingStrategy.cs
+++ Assets/_Project/Scripts/Gameplay/Turrets/TurretTargetingStrategy.cs
@@ -4,6 +4,10 @@ namespace _Project.Scripts.Gameplay.Turrets
 {
     public abstract class TurretTargetingStrategy : ScriptableObject
     {
-        public abstract void GetTarget();
+        public float Range;
+
+        public abstract void Initialize(TurretTargeting turretTargeting);
+        
+        public abstract Transform GetSingleTarget();
     }
 }
